34fe3d8e412fdb8c7ff019e3a94c95d92a06d2b7
Mona Lisa <naomiroznik@gmail.com>
2022-11-02 08:30:58 -0600

    Adding the save button in js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 var saveButton = document.getElementsByClassName("saveBtn");
 var currentDay = moment().format('dddd MMMM Do YYYY');
 $("#currentDay").html(currentDay);
+
+$(document).ready(function() {
+    $(".saveBtn").on("click", function () {
+        var textBlock = $(this).siblings(".description").val();
+        var timeBlock = $(this).parent().attr("id");
+    
+    localStorage.setItem(timeBlock, textBlock);
+    });
+    });
+    
+    function trackTime() {
+        var currentTime =  moment().hour();
